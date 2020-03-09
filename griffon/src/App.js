d9763fd09a66874a6c409d13701f4f105fe5ba73
jackth <19895932+jackthb@users.noreply.github.com>
2020-03-09 11:45:30 +0000

    changes

--- griffon/src/App.js
+++ griffon/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Let's go!</p>
+        <p>hello world</p>
       </header>
     </div>
   );
