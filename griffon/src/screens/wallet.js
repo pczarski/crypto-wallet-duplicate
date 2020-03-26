03b8e1ebf965981119aa00dc097b81ccbfd70ff4
1min2midnight <mendezdemian@gmail.com>
2020-03-26 18:52:19 +0000

    added currency logos to wallet page

--- griffon/src/screens/wallet.js
+++ griffon/src/screens/wallet.js
@@ -5,9 +5,15 @@ import {Link} from 'react-router-dom';
 import '../styles/App.scss';
 import '../styles/nav.scss';
 import '../styles/bal.scss';
+import '../styles/coinLogos.css';
 import {getRequest} from '../lib/backendHandler.js';
-
+//---import images for each currency---
+import bitcoinLogo from '../assets/bitcoinLogo.png';
+import ethLogo from '../assets/ethereum.png';
+import dashLogo from '../assets/Dash.png';
+//---------
 import { Button } from 'reactstrap';
+
 
 export default class Wallet extends React.Component {
 
@@ -26,7 +32,15 @@ export default class Wallet extends React.Component {
             <div className='d-flex flex-row justify-content-around'>
               <Button className="btn btn-primary" size="lg">Send</Button>
               <Button type="button" className="btn btn-primary" size="lg">Receive</Button>
+
             </div>
+                 <h3> list of currencies</h3>
+
+                  <img src={bitcoinLogo} alt= "Bitcoin"></img>
+                  <img src={ethLogo} alt="Ethereum"></img>
+                  <img src={dashLogo} alt = "Dash"></img>
+
+
               <Link to="/">
                 <Button className="btn btn-primary" size="lg" block>Go back</Button>
               </Link>
