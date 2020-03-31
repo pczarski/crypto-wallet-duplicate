3a9038366e96aec69c55c59be01483f07d53b499
Jack Burgess <198895932+jackth@users.noreply.github.com>
2020-03-31 17:36:08 +0100

    made a function to call specific currencies, added prices to cards on wallet page

--- griffon/src/screens/wallet.js
+++ griffon/src/screens/wallet.js
@@ -6,13 +6,17 @@ import '../styles/App.scss';
 import '../styles/nav.scss';
 import '../styles/bal.scss';
 import '../styles/coinLogos.css';
-import {getRequest} from '../lib/backendHandler.js';
+import {getRequest, getCurr} from '../lib/backendHandler.js';
+
 //---import images for each currency---
+
 import bitcoinLogo from '../assets/bitcoinLogo.png';
 import ethLogo from '../assets/ethereum.png';
 import dashLogo from '../assets/Dash.png';
 import liteLogo from '../assets/litecoin.png';
+
 //---------
+
 import { Button } from 'reactstrap';
 
 
@@ -33,38 +37,34 @@ export default class Wallet extends React.Component {
             <div className='d-flex flex-row justify-content-around'>
               <Button className="btn btn-primary" size="lg">Send</Button>
               <Button type="button" className="btn btn-primary" size="lg">Receive</Button>
-
             </div>
-
-                 <h3> list of currencies</h3>
+            <h3> list of currencies</h3>
             <div class = "row">
                     <div class = "bitcoin-container">
                         <img src={bitcoinLogo} alt= "Bitcoin"></img>
                         <div class ="bitcoin-overlay">
-                            <div class="bitcoin-price">insert bitcoin balance here</div>
+                          <div class="bitcoin-price">{getCurr("Bitcoin").price}</div>
                         </div>
                     </div>
                     <div class ="ethereum-container">
                         <img src={ethLogo} alt="Ethereum"></img>
                         <div class ="ethereum-overlay">
-                            <div class ="ethereum-price"> insert ethereum balance here</div>
+                            <div class ="ethereum-price">{getCurr("Ethereum").price}</div>
                         </div>
                      </div>
                     <div class ="dash-container">
                         <img src={dashLogo} alt = "Dash"></img>
                         <div class = "dash-overlay">
-                            <div class ="dash-price"> insert dash balance here</div>
+                            <div class ="dash-price">{getCurr("Dash").price}</div>
                          </div>
                      </div>
                     <div class ="litecoin-container">
                         <img src={liteLogo} alt = "LiteCoin"></img>
                         <div class = "litecoin-overlay">
-                            <div class ="litecoin-price"> insert litecoin balance here</div>
+                            <div class ="litecoin-price">{getCurr("Litecoin").price}</div>
                         </div>
                     </div>
              </div>
-
-
               <Link to="/">
                 <Button className="btn btn-primary" size="lg" block>Go back</Button>
               </Link>
