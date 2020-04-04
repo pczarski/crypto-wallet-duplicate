aa3c1bd4985d38e2f5bd635204da8a69e54fb819
Rakib Ali <r.ali@se18.qmul.ac.uk>
2020-04-04 22:01:41 +0100

    Added Form validation
    form will check password for a number of conditions

--- griffon/src/screens/settings.js
+++ griffon/src/screens/settings.js
@@ -10,7 +10,62 @@ import '../styles/settings.scss';
 
 import { Button, Alert } from 'reactstrap';
 
+const initialState ={
+  current:"",
+  new:"",
+  confirm:"",
+  currentError:"",
+  newError:"",
+  confirmError:""
+
+}
+const userpass="hello";
 export default class Settings extends React.Component {
+
+  state=initialState;
+  handleChange = event=>{
+    const isCheckbox = event.target.type === "checkbox";
+   this.setState({
+     [event.target.name]: isCheckbox
+       ? event.target.checked
+       : event.target.value
+   });
+ };
+  validate=()=>{
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+    let password1= userpass;
+    let currentError="";
+    let newError="";
+    let confirmError="";
+    if(!this.state.current.includes(password1)){
+      currentError="Password is not correct!";
+    }
+    if(this.state.confirm!=""){
+    if(this.state.confirm != this.state.new){
+      confirmError="Please make sure your passwords match!";
+
+    }
+    }
+    if(this.state.new =="" || this.state.new.length <8){
+      newError="Password should contain atleast 8 characters, one capital letter and one digit";
+  }
+
+
+    if (currentError || confirmError || newError){
+      this.setState({currentError, confirmError, newError})
+      return false;
+    }
+
+    return true;
+  }
+  handleSubmit=event=>{
+    event.preventDefault();
+    const isValid = this.validate();
+    if(isValid){
+    console.log(this.state)
+    this.setState(initialState);
+  }
+  }
   render () {
     return (
 
@@ -28,24 +83,26 @@ export default class Settings extends React.Component {
            </ul>
            </nav>
            <div className="extra">
-         <form>
+         <form onSubmit={this.handleSubmit}>
 
          <h1>Change Password</h1>
          <div>
          <p>Enter your current Password:</p>
-         <input type='text' name="password" placeholder="Current password"/>
+         <input type='password' name="current" placeholder="Current password" value={this.state.current} onChange={this.handleChange}/>
          </div>
+         <div style={{ color:"#f73636"}}>{this.state.currentError}</div>
          <div>
          <p>Enter your new Password:</p>
-         <input type='text' name="password" placeholder="New password"/>
+         <input type='password' name="new" placeholder="New password" value={this.state.new} onChange={this.handleChange}/>
          </div>
+          <div style={{ color:"#f73636"}}>{this.state.newError}</div>
          <div>
          <p>Confirm your new Password:</p>
-         <input type='text' name="password" placeholder="Confirm password"/>
-
+         <input type='password' name="confirm" placeholder="Confirm password" value={this.state.confirm} onChange={this.handleChange}/>
          </div>
+          <div style={{ color:"#f73636"}}>{this.state.confirmError}</div>
          <div className="password">
-         <Button type="button" size="md" className="btn btn-primary">Change Password</Button>
+         <Button type="submit" size="md" className="btn btn-primary">Change Password</Button>
          </div>
          </form>
            </div>
