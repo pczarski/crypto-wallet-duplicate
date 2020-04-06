7e6d12da0e5a8a8b29eebef67e5fe4e36e405bfc
1min2midnight <mendezdemian@gmail.com>
2020-04-06 04:58:50 +0100

    Accordion style is now working for all four cryptos supported

--- griffon/src/components/Accordion.js
+++ griffon/src/components/Accordion.js
@@ -1,46 +1,106 @@
 import React from "react";
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import { Collapse, Button, CardBody, Card, CardTitle } from 'reactstrap';
+import "../styles/Accordion.css";
 
-
+//-----coin Logos-------
+import ethLogo from "../../node_modules/cryptocurrency-icons/svg/color/eth.svg";
+import dashLogo from "../../node_modules/cryptocurrency-icons/svg/color/dash.svg";
+import liteLogo from "../../node_modules/cryptocurrency-icons/svg/color/ltc.svg";
+import bitcoinLogo from "../../node_modules/cryptocurrency-icons/svg/color/btc.svg";
+//------------------
 export default class Accordion extends React.Component {
 
     constructor(props) {
         super(props);    
-        this.toggle = this.toggle.bind(this);
+        
         this.state = {
-          isOpen: false
+            rSelected:null,
+            
+        }
+      }
+    
+    toggle =(id)=>{
+        
+        this.setState({
+            rSelected:id,
+           
 
-
+        })
+        console.log(id)
+    }
+    //handles which button is selected
+    isSelected=(id)=>{
+        if(id == this.state.rSelected){
+            return true;
 
         }
-    
-      }
-    toggle(e) {
-        this.setState(
-            {
-                isOpen: !this.state.isOpen
-            }
-        )
-
-
+        else{
+            return false
+        }
     }
 
     render(){
+        
         return(
-            <div>
-                <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-                <Collapse isOpen={this.state.isOpen}>
-                    <Card>
-                    <CardBody>
-                    Anim pariatur cliche reprehenderit,
-                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
-                    anim keffiyeh helvetica, craft beer labore wes anderson cred
-                    nesciunt sapiente ea proident.
-                    </CardBody>
-                    </Card>
-                </Collapse>
-            </div>
+            <div className = "container">
+                
+                
+                            <div className= "BitCoin">
+                                <Button color="primary" onClick={() => this.toggle(1)} block><img src={bitcoinLogo} alt= "Bitcoin"></img>BitCoin</Button>
+                                <Collapse isOpen={this.isSelected(1)}>
+                                    <Card>
+                                    <CardBody>
+                                    Anim pariatur cliche reprehenderit,
+                                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    nesciunt sapiente ea proident.
+                                    </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
 
+                            <div className= "Ethereum">
+                                <Button color="primary" onClick={() => this.toggle(2)} block><img src={ethLogo} alt= "Ethereum" ></img>Ethereum</Button>
+                                <Collapse isOpen={this.isSelected(2)}>
+                                    <Card>
+                                    <CardBody>
+                                    Anim pariatur cliche reprehenderit,
+                                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    nesciunt sapiente ea proident.
+                                    </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
+                            <div className= "LiteCoin">
+                                <Button color="primary" onClick={() => this.toggle(3)} block><img src={liteLogo} alt= "LiteCoin" ></img>LiteCoin</Button>
+                                <Collapse isOpen={this.isSelected(3)}>
+                                    <Card>
+                                    <CardBody>
+                                    Anim pariatur cliche reprehenderit,
+                                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    nesciunt sapiente ea proident.
+                                    </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
+                            <div className= "Dash">
+                                <Button color="primary" onClick={() => this.toggle(4)} block><img src={dashLogo} alt= "Dash" ></img>Dash</Button>
+                                <Collapse isOpen={this.isSelected(4)}>
+                                    <Card>
+                                    <CardBody>
+                                    Anim pariatur cliche reprehenderit,
+                                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    nesciunt sapiente ea proident.
+                                    </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
+                
+            </div>
+                
         );
 
 
