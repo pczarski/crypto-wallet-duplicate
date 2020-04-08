a4cf3a1544d1b39c810c3ad594d3c2847d35f73d
Przemyslaw Czarski <p.czarski@se19.qmul.ac.uk>
2020-04-08 22:49:56 +0100

    Order History implemented

--- cryapi/src/main/java/g37/cryapi/exchange/api/ExchangeController.java
+++ cryapi/src/main/java/g37/cryapi/exchange/api/ExchangeController.java
@@ -1,5 +1,6 @@
 package g37.cryapi.exchange.api;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
 import g37.cryapi.common.CryptoCurrency;
 import g37.cryapi.common.TextResponse;
 import g37.cryapi.exchange.*;
@@ -7,6 +8,8 @@ import g37.cryapi.wallet.Wallet;
 import org.springframework.web.bind.annotation.*;
 
 import java.rmi.NoSuchObjectException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ExchangeController {
@@ -135,15 +138,16 @@ public class ExchangeController {
                     amount, price
                     );
             return new OrderJson(
-                    order.getOrderID(), order.getCurrency1().toString(), order.getCurrency2().toString(),
+                    order.getOrderID(), order.getExchangeAccess().getName().getName(),
+                    order.getCurrency1().toString(), order.getCurrency2().toString(),
                     order.getInitialAmount(), order.getAmountComplete(), order.getUnitPrice(),
                     order.getType(), order.getStatus(), order.getDate().toString()
                     );
         } catch (IllegalArgumentException e) {
-            return new OrderJson(-1, "Unsupported exchange or currency", null, -1,
+            return new OrderJson(-1, null, "Unsupported exchange or currency", null, -1,
                     -1, -1, null, null, null);
         } catch (IllegalStateException e) {
-            return new OrderJson(0, "Insufficient Balance", null, -1,
+            return new OrderJson(0, null, "Insufficient Balance", null, -1,
                     -1, -1, null, null, null);
         }
 
@@ -165,15 +169,16 @@ public class ExchangeController {
                     CryptoCurrency.valueOf(currency2), amount
             );
             return new OrderJson(
-                    order.getOrderID(), order.getCurrency1().toString(), order.getCurrency2().toString(),
+                    order.getOrderID(), order.getExchangeAccess().getName().getName(),
+                    order.getCurrency1().toString(), order.getCurrency2().toString(),
                     order.getInitialAmount(), order.getAmountComplete(), order.getUnitPrice(),
                     order.getType(), order.getStatus(), order.getDate().toString()
             );
         } catch (IllegalArgumentException e) {
-            return new OrderJson(-1, "Unsupported exchange or currency", null, -1,
+            return new OrderJson(-1, null, "Unsupported exchange or currency", null, -1,
                     -1, -1, null, null, null);
         } catch (IllegalStateException e) {
-            return new OrderJson(0, "Insufficient Balance", null, -1,
+            return new OrderJson(0, null, "Insufficient Balance", null, -1,
                     -1, -1, null, null, null);
         }
 
@@ -190,18 +195,57 @@ public class ExchangeController {
         try {
             Order order = exchangeHandler.getOrder(id, ExchangeName.valueOf(exchange));
             return new OrderJson(
-                    order.getOrderID(), order.getCurrency1().toString(), order.getCurrency2().toString(),
+                    order.getOrderID(), order.getExchangeAccess().getName().getName(),
+                    order.getCurrency1().toString(), order.getCurrency2().toString(),
                     order.getInitialAmount(), order.getAmountComplete(), order.getUnitPrice(),
                     order.getType(), order.getStatus(), order.getDate().toString()
             );
         } catch (IllegalArgumentException e) {
-            return new OrderJson(-1, "Unsupported exchange or currency", null, -1,
+            return new OrderJson(-1, null, "Unsupported exchange or currency", null, -1,
                     -1, -1, null, null, null);
         } catch (NoSuchObjectException e) {
-            return new OrderJson(0, "Order not found", null, -1,
+            return new OrderJson(0, null, "Order not found", null, -1,
                     -1, -1, null, null, null);
         }
-
     } // http://localhost:8080/get-order?&exchange=Binance&id=1
+
+    @CrossOrigin(origins = "*")  //fixes the CORS blocking problem
+    @GetMapping("/all-orders") // setting up the url location
+    public OrderJson[] getOrders() {
+        this.runHelpers();
+        ExchangeHandler exchangeHandler = ExchangeHandler.getInstance();
+        return this.getOrderList(exchangeHandler.getFullOrderHistory());
+    } // http://localhost:8080/all-orders
+
+    @CrossOrigin(origins = "*")  //fixes the CORS blocking problem
+    @GetMapping("/orders") // setting up the url location
+    public OrderJson[] getExchangeOrders(@RequestParam(value = "exchange") String exchange) {
+        this.runHelpers();
+        ExchangeHandler exchangeHandler = ExchangeHandler.getInstance();
+        try {
+            return this.getOrderList(exchangeHandler.getExchange(ExchangeName.valueOf(exchange)).getOrders());
+        } catch (IllegalArgumentException e) {
+            OrderJson[] toRet = {new OrderJson(-1, null, "Unsupported/Invalid exchange", null, -1,
+                    -1, -1, null, null, null)};
+            return toRet;
+        }
+    } // http://localhost:8080/orders?exchange=Binance
+
+
+    private OrderJson[] getOrderList(List<Order> orderList) {
+        OrderJson[] toRet = new OrderJson[orderList.size()];
+        for(int i = 0; i < toRet.length; i++) {
+            Order order = orderList.get(i);
+            toRet[i] = new OrderJson(
+                    order.getOrderID(), order.getExchangeAccess().getName().getName(),
+                    order.getCurrency1().toString(), order.getCurrency2().toString(),
+                    order.getInitialAmount(), order.getAmountComplete(), order.getUnitPrice(),
+                    order.getType(), order.getStatus(), order.getDate().toString()
+            );
+        }
+        return toRet;
+    }
+
+
 
 }
