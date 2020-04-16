e0a110131e6fcefde2a4c6651597008c68c23b59
Przemyslaw Czarski <p.czarski@se19.qmul.ac.uk>
2020-04-16 16:21:34 +0100

    click on row now takes you to the right page on transfer

--- griffon/src/screens/wallet.js
+++ griffon/src/screens/wallet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from '../components/Nav';
-
+import {Redirect} from 'react-router';
 import {Link} from 'react-router-dom';
 import '../styles/App.scss';
 import '../styles/nav.scss';
@@ -10,8 +10,6 @@ import '../styles/bal.scss';
 
 import Coins from '../components/walletComponents/Coins.js';
 
-import {getCurr} from '../lib/backendHandler.js';
-
 import { Button } from 'reactstrap';
 
 export default class Wallet extends React.Component {
@@ -20,18 +18,33 @@ export default class Wallet extends React.Component {
     this.state= {
       supportedCurr: ["BTC", "ETH", "DASH", "LTC", 	"USDT"],
       currency: [],
-      seed: null
+      seed: null,
+      goToTransfer: false,
     }
   }
 
+  handleCoinClick = (coin) => {
+    this.props.handleCoinClick(coin);
+    console.log(coin);
+    this.setState({
+      goToTransfer: true,
+    });
+  };
+
   render () {
+    const transfer = this.state.goToTransfer;
+    if (transfer) {
+      return (
+          <Redirect to="/transfer"/>
+      )
+    }
 
     return (
       <div className="wrapper">
       <Nav />
         <div className="container">
           <div className="content">
-              <Coins fetch={this.props.fetch} coins={this.props.coins} />
+              <Coins fetch={this.props.fetch} coins={this.props.coins} coinClick={this.handleCoinClick}/>
             <Link to="/transfer">
               <Button className="btn btn-primary" size="lg" block>Send or Receive Currency</Button>
             </Link>
