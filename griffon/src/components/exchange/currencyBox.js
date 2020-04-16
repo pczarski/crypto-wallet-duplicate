e877a0c19cbb251a462b03074064af0ef6c48db6
Przemyslaw Czarski <p.czarski@se19.qmul.ac.uk>
2020-04-16 23:12:31 +0100

    Update currencyBox.js

--- griffon/src/components/exchange/currencyBox.js
+++ griffon/src/components/exchange/currencyBox.js
@@ -4,7 +4,7 @@ export default function CurrencyBox(props) {
 
     return(
         <div>
-            TODO: make a currency box that allows you to specify amount, or displays given amount
+            TODO: currency box that allows you to select currency, specify amount, or displays given amount
         </div>
     )
 }
