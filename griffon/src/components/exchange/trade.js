6cafef02b40adb7069d60aa4c333cf887dc76f9a
Przemyslaw Czarski <p.czarski@se19.qmul.ac.uk>
2020-04-19 19:28:48 +0100

    Update trade.js

--- griffon/src/components/exchange/trade.js
+++ griffon/src/components/exchange/trade.js
@@ -96,14 +96,14 @@ export default class Trade extends React.Component {
         e.preventDefault();
         if(this.props.amount <= 0){
             this.setState({
-                response: "Specify an amount grater than 0",
+                response: "specify an amount grater than 0",
                 isError: true,
             });
             return;
         }
         if(this.props.price <= 0){
             this.setState({
-                response: "Specify a price grater than 0",
+                response: "specify a price grater than 0",
                 isError: true,
             });
             return;
